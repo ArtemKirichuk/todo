ef171cae324a26d04ae44e2c9dad00f09ea5fa43
Kirichuk Artem <mfstarosta@gmail.com>
2022-06-01 15:50:01 +0300

    add rout start and tasks page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StartpageComponent } from './startpage/startpage.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartpageComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: StartpageComponent },
+      { path: 'task', component: TasksComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
