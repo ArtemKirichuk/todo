acd3f462ac1031f38830bd5da0dab6e127b57c72
Kirichuk Artem <mfstarosta@gmail.com>
2022-06-06 12:34:12 +0300

    dilog delete task

---
+++ src/app/tasks/dilog-delete-task/dilog-delete-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dilog-delete-task',
+  templateUrl: './dilog-delete-task.component.html',
+  styleUrls: ['./dilog-delete-task.component.scss']
+})
+export class DilogDeleteTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
